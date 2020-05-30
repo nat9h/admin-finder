49aa242ba80462adeac30d925f6f3afb8230f67a
nat9h <suryaarj58@gmail.com>
2020-05-31 00:42:19 +0800

    Admin Finder Tool

--- admin-finder.php
+++ admin-finder.php
@@ -18,31 +18,31 @@ echo " \033[93m
 
 echo "\033[91m Url Target : ";
 $target = trim(fgets(STDIN));
-$list 	= "wordexploit.txt";
-//$list = trim(fgets(STDIN));
+echo "\033[91m List file : ";
+$list = trim(fgets(STDIN));
 if(!preg_match("/^http:\/\//",$target) AND !preg_match("/^https:\/\//",$target)){
 	$targets = "http://$target";
 }else{
 	$targets = $target;
 }
 
-$buka 	= fopen("$list","r");
-$ukuran = filesize("$list");
-$baca 	= fread($open,$size);
-$lists 	= explode("\r\n",$read);
+$open = fopen("$list","r");
+$size = filesize("$list");
+$read = fread($open,$size);
+$lists = explode("\r\n",$read);
 
 foreach($lists as $login){
 	$admins = "$targets/$login";
 	
-	$ch = curl_init("$log");
+	$ch = curl_init("$admins");
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_exec($ch);
 	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 	curl_close($ch);
 	if($httpcode == 200){
-		echo "\n\n \033[92m Cek $log => Ditemukan!\n";
+		echo "\n\n \033[92m Cek $admins => Ditemukan!\n";
 	}else{
-		echo "\n \033[94m Cek $log => Tidak di ditemukan ~";
+		echo "\n \033[94m Cek $admins => Tidak ditemukan ~";
 	}
 }
