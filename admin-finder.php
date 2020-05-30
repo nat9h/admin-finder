b261148b5a8222b40934159e7606371e0714023c
nat9h <suryaarj58@gmail.com>
2020-05-31 01:11:07 +0800

    Admin Finder Tool

--- admin-finder.php
+++ admin-finder.php
@@ -5,20 +5,20 @@
 
 echo " \033[93m
 
-			 _     __      _   _    __  __
-			(_) /\ \ \__ _| |_| |__ \ \/ /
-			| |/  \/ / _` | __| '_ \ \  / 
-			| / /\  / (_| | |_| | | |/  \ 
-			|_\_\ \/ \__,_|\__|_| |_/_/\_\
+     	 _     __      _   _    __  __
+     	(_) /\ \ \__ _| |_| |__ \ \/ /
+     	| |/  \/ / _` | __| '_ \ \  / 
+     	| / /\  / (_| | |_| | | |/  \ 
+     	|_\_\ \/ \__,_|\__|_| |_/_/\_\
                               
 
-          [ Admin Finder Recoded by iNathX  ]
+      [ Admin Finder Recoded by iNathX  ]
 
 ";
 
 echo "\033[91m Url Target : ";
 $target = trim(fgets(STDIN));
-echo "\033[91m List file : ";
+echo "\033[91m List file  : ";
 $list = trim(fgets(STDIN));
 if(!preg_match("/^http:\/\//",$target) AND !preg_match("/^https:\/\//",$target)){
 	$targets = "http://$target";
@@ -41,8 +41,9 @@ foreach($lists as $login){
 	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 	curl_close($ch);
 	if($httpcode == 200){
-		echo "\n\n \033[92m Cek $admins => Ditemukan!\n";
+		$handle = fopen("result.txt", "a+");
+		echo "\n\n \033[92m [".date('H:m:s')."] Cek $admins => Ditemukan!\n";
 	}else{
-		echo "\n \033[94m Cek $admins => Tidak ditemukan ~";
+		echo "\n \033[94m [".date('H:m:s')."] Cek $admins => Tidak ditemukan ~";
 	}
 }
